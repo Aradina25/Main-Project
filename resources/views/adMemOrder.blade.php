0c7126e048a4b66ce4c1ad0389dbc871bbb6a98b
Aradina25 <aradinababu3669@gmail.com>
2023-02-01 05:07:52 +0100

    commit

---
+++ resources/views/adMemOrder.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <title>adminDasboard</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('addash.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jquery-3.6.0.min.js"></script>
+    <script type="text/javascript">
+    </script>
+    <style>
+        body{
+            font-family: cursive;
+            background-color:rgba(6, 6, 6, 0.797);
+        }
+    </style>
+
+</head>
